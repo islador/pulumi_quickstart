f6be2cb1d9c0e901f3624c32197513b773cad330
Luke Mercado <luke.isla@gmail.com>
2024-11-02 19:44:09 -0700

    Got stuck on setting the access policies to allow public access, it's quite frustrating really. I might skip over this since I likely won't need public access for most of my data. I do want to know what I'm stumbling on though since the debugging skill is likely to be required in the future.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ GCP Project: luke@lukemercado.com/pulumiQuickstart
 ** Configured via `GOOGLE_CLOUD_PROJECT` env variable
 ** ```export GOOGLE_CLOUD_PROJECT=pulumiQuickstart```
 
+## GCP Weirdness
+The IAM system has some global roles, `allUsers` is a reserved scope akin to amazon's `public` per https://cloud.google.com/storage/docs/access-control/lists
+
--- index.ts
+++ index.ts
@@ -5,7 +5,33 @@ import * as gcp from "@pulumi/gcp";
 const bucket = new gcp.storage.Bucket("my-bucket", {
     location: "US",
     uniformBucketLevelAccess: true,
+    publicAccessPrevention: "inherited"
 });
+
+// Put something in the bucket
+const bucketObject = new gcp.storage.BucketObject("index.html", {
+    bucket: bucket.name,
+    source: new pulumi.asset.FileAsset("index.html")
+});
+
+// This approach didn't work with the same error, initial searchings say that there's a different thing called domain restricted sharing which might be at play. It's unclear to me what that is.
+const bucketPolicyData = gcp.organizations.getIAMPolicy({
+    bindings: [{
+        role: "roles/storage.objectViewer",
+        members: ["allUsers"]
+    }],
+})
+
+const bucketPolicy = new gcp.storage.BucketIAMPolicy("policy", {
+    bucket: bucket.name,
+    policyData: bucketPolicyData.then(bucketPolicyData => bucketPolicyData.policyData),
+});
+//Set policy on the bucket, it's a binding?
+//const bucketBinding = new gcp.storage.BucketIAMBinding("my-bucket-binding", {
+//    bucket: bucket.name,
+//    role: "roles/storage.objectViewer",
+//    members: ["allUsers"]
+//});
 
 // Export the DNS name of the bucket
 export const bucketName = bucket.url;
